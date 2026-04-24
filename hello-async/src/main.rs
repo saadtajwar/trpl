49bba9e0bf6ba87ebda50da842c94f2115a86e00
Saad Tajwar <saads@ad.unc.edu>
2026-04-24 18:10:51 -0400

    finally

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -2,23 +2,41 @@ use trpl::{Either, Html};
 use std::time::Duration;
 
 fn main() {
-    trpl::block_on(async {
-        let fut1 = async {
-            for i in 1..10 {
-                println!("hi number {i} from the first task!");
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
-        };
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
 
-        let fut2 = async {
-            for i in 1..5 {
-                println!("hi number {i} from the second task!");
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
-        };
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color {color} as background");
+    } else if is_tuesday {
+        println!("tuesday is green day");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("purple is background color");
+        } else {
+            println!("orange is background color");
+        }
+    } else {
+        println!("blue is background color");
+    }
 
-        trpl::join(fut1, fut2).await;
-    });
+    // trpl::block_on(async {
+    //     let fut1 = async {
+    //         for i in 1..10 {
+    //             println!("hi number {i} from the first task!");
+    //             trpl::sleep(Duration::from_millis(500)).await;
+    //         }
+    //     };
+
+    //     let fut2 = async {
+    //         for i in 1..5 {
+    //             println!("hi number {i} from the second task!");
+    //             trpl::sleep(Duration::from_millis(500)).await;
+    //         }
+    //     };
+
+    //     trpl::join(fut1, fut2).await;
+    // });
 
     // let args: Vec<String> = std::env::args().collect();
     // trpl::block_on(async {
